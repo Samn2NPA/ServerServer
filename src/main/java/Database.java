af6b550d4879d66c301977cc8debdc93178ac635
N.N.P.A <anh.nguyen2@mservice.com.vn>
2020-11-27 18:20:04 +0700

    [feature/database-docker] - add db mysql - docker: mysql

---
+++ src/main/java/Database.java
@@ -0,0 +1,70 @@
+import Common.Log;
+
+import java.sql.*;
+
+/**
+ * Created by Phanh on 11/11/20.
+ */
+public class Database {
+    static Log log = new Log(Database.class.getSimpleName());
+
+    private Connection connect = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+
+    public void readDataBase() throws Exception {
+        try {
+            // This will load the MySQL driver, each DB has its own driver
+            Class.forName("com.mysql.jdbc.Driver");
+            // Setup the connection with the DB
+            connect = DriverManager
+                    .getConnection("jdbc:mysql://192.168.99.1:3106/?useSSL=false&allowPublicKeyRetrieval=true", "root", "#uJugf0kwitdiq#4P0nAGnEm@qe");
+            log.info("db is started");
+
+            // Statements allow to issue SQL queries to the database
+            statement = connect.createStatement();
+            // Result set get the result of the SQL query
+            resultSet = statement
+                    .executeQuery("select * from user");
+
+            log.info(String.format("RESULT SET size = %d", resultSet.getFetchSize()));
+            writeMetaData(resultSet);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            close();
+        }
+
+    }
+
+    private void writeMetaData(ResultSet resultSet) throws SQLException {
+        //  Now get some metadata from the database
+        // Result set get the result of the SQL query
+
+        System.out.println("The columns in the table are: ");
+
+        System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
+        for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+            System.out.println("Column " + i + " " + resultSet.getMetaData().getColumnName(i));
+        }
+    }
+
+    private void close() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (statement != null) {
+                statement.close();
+            }
+
+            if (connect != null) {
+                connect.close();
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
--- src/main/java/RunServer.java
+++ src/main/java/RunServer.java
@@ -9,11 +9,20 @@ public class RunServer {
     static Log log = new Log(RunServer.class.getSimpleName());
 
     public static void main(String[] args) {
-        HTTPServer httpServer = new HTTPServer();
+       /* HTTPServer httpServer = new HTTPServer();
         log.info(" HTTP server processing...");
         httpServer.start();
 
         SimpleTCPServer tcpServer = new SimpleTCPServer();
-        log.info(" SimpleTCPServer processing...");
+        log.info(" SimpleTCPServer processing...");*/
+
+        Database myDb = new Database();
+        try {
+            myDb.readDataBase();
+            log.info("start ok");
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("EXCEPTION occurs");
+        }
     }
 }
